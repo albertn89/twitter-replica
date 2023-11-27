25d3d51f5979cbbe93b363ecf518e325441b9f63
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2023-11-26 21:57:48 -0700

    User Auth logic added

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,15 +1,37 @@
+import { useCallback } from "react";
+import { useRouter } from "next/router";
 import { IconType } from "react-icons";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
 
 interface SidebarItemProps {
 	label: string;
 	href?: string;
 	icon: IconType;
 	onClick?: () => void;
+	auth?: boolean;
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ label, href, icon: Icon, onClick }) => {
+const SidebarItem: React.FC<SidebarItemProps> = ({ label, href, icon: Icon, onClick, auth }) => {
+	const { data: currentUser } = useCurrentUser();
+	const loginModal = useLoginModal();
+	const router = useRouter();
+
+	const handleClick = useCallback(() => {
+		if (onClick) {
+			return onClick();
+		}
+
+		if (auth && !currentUser) {
+			loginModal.onOpen();
+		} else if (href) {
+			router.push(href);
+		}
+	}, [router, onClick, href, currentUser, auth, loginModal]);
+
 	return (
-		<div className="flex flex-row items-center">
+		<div onClick={handleClick} className="flex flex-row items-center">
 			<div
 				className="
 					relative 
